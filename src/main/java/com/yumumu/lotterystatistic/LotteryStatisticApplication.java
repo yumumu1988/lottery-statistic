4089ab59737294d13a239d7120432dae78667e49
zhanghailin <zhanghailin@aixuexi.com>
2022-10-26 17:56:12 +0800

    initial code

---
+++ src/main/java/com/yumumu/lotterystatistic/LotteryStatisticApplication.java
@@ -0,0 +1,13 @@
+package com.yumumu.lotterystatistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryStatisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryStatisticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yumumu/lotterystatistic/LotteryStatisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yumumu.lotterystatistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryStatisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
